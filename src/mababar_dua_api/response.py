a2e2bffa2dcd719ebb89825bd3efb3de2f101790
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-05-05 13:55:05 +0600

    remove redundent _build_headers function from subclass

--- src/mababar_dua_api/response.py
+++ src/mababar_dua_api/response.py
@@ -81,14 +81,6 @@ class JSONResponse(Response):
             headers=headers,
         )
 
-    def _build_headers(self) -> list[tuple[bytes, bytes]]:
-        headers: dict[str, str] = {"content-type": "application/json"}
-        headers.update(self._extra_headers)
-        return [
-            (k.lower().encode("latin-1"), v.encode("latin-1"))
-            for k, v in headers.items()
-        ]
-
 
 class HTMLResponse(Response):
     media_type = "text/html"
@@ -104,11 +96,3 @@ class HTMLResponse(Response):
             status_code=status_code,
             headers=headers,
         )
-
-    def _build_headers(self) -> list[tuple[bytes, bytes]]:
-        headers: dict[str, str] = {"content-type": "text/html; charset=utf-8"}
-        headers.update(self._extra_headers)
-        return [
-            (k.lower().encode("latin-1"), v.encode("latin-1"))
-            for k, v in headers.items()
-        ]
